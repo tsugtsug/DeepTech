b69d8ad2d7380dac3bebd035dcc1c5ff435abf60
tsugtsug <98301922+tsugtsug@users.noreply.github.com>
2024-09-25 22:09:25 +0200

    Add files via upload

---
+++ rx.ino
@@ -0,0 +1,32 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  // LoRaモジュールを初期化
+  Serial.println("LoRa Receiver");
+  if (!LoRa.begin(915E6)) {  // 送信側と同じ周波数（915 MHz）でLoRa通信を設定
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+}
+
+void loop() {
+  // パケットを受信したかどうかを確認
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // パケットを受信
+    Serial.print("Received packet: ");
+
+    // 受信したメッセージを表示
+    while (LoRa.available()) {
+      String message = LoRa.readString();
+      Serial.print(message);
+    }
+
+    Serial.print(" with RSSI: ");
+    Serial.println(LoRa.packetRssi());  // 受信信号強度（RSSI）を表示
+  }
+}

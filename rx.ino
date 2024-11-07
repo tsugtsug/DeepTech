d2e8cda4a0d90776dc7b3cf56b3332bca2c92fff
tsugtsug <98301922+tsugtsug@users.noreply.github.com>
2024-11-07 15:08:45 +0100

    Update rx.ino
    Modified the logic of LED light

--- rx.ino
+++ rx.ino
@@ -5,10 +5,10 @@ void setup() {
   Serial.begin(9600);
   while (!Serial);
 
-  // LoRaモジュールを初期化
+  // initialize the LoRa module
   Serial.println("LoRa Receiver");
   while (1) {
-    if (!LoRa.begin(868E6)) {  // 送信側と同じ周波数（868 MHz）でLoRa通信を設定
+    if (!LoRa.begin(868E6)) {  // LoRa communication 868 MHz
       Serial.println("Starting LoRa failed!");
     }
     else {
@@ -17,42 +17,62 @@ void setup() {
     }
     delay(1000);
   }
-  // GND 3本目
-  // 6 2本目 
-  pinMode(6, OUTPUT);
-  // 7 4本目
-  pinMode(7, OUTPUT);
 
+  // Set each pin
+  // GND 3本目
+  // 6 2本目 RED
+  pinMode(6, OUTPUT);
+  // 7 4本目 GREEN
+  pinMode(7, OUTPUT);
+  // 8 1本目 BLUE
+  pinMode(8, OUTPUT);
+
+  // Initialize all the digitalPins off
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
 }
 
 void loop() {
-  // パケットを受信したかどうかを確認
-  int packetSize = LoRa.parsePacket();
-  if (packetSize) {
-    // パケットを受信
-    // Serial.print("Received packet: ");
+  int packetSize = LoRa.parsePacket(); // Recieve LoRa packet
+  if (packetSize) { // If it receives a LoRa packet
 
-    String message;
-    // 受信したメッセージを表示
+    String message; // Variable for the message that it receives
     while (LoRa.available()) {
       message = LoRa.readString();
     }
-    if (message == "0") {
-      // GREEN
-      Serial.println("open");
-      digitalWrite(6, LOW);
-      digitalWrite(7, HIGH);
-    } else if (message == "1") {
-      // RED
-      Serial.println("close");
-      digitalWrite(7, LOW);
-      digitalWrite(6, HIGH);
-    } else {
-      Serial.println("error" + message);
-    }
 
-    delay(10);
-    // Serial.print(" with RSSI: ");
-    // Serial.println(LoRa.packetRssi());  // 受信信号強度（RSSI）を表示
+    // Switch
+
+    switch (message[0]) {
+      case '0': // 6 RED magnetic sensor on
+        Serial.println("The door is closed. Someone is taking a shower");
+        digitalWrite(6, HIGH); // 6 HIGH
+        digitalWrite(7, LOW);
+        digitalWrite(8, LOW);
+        break;
+
+      case '1': // 8 BLUE magnetic sensor off and the temperature is more than 25 degrees
+        Serial.println("The door is open. Someone forgot to close the door");
+        digitalWrite(6, LOW);
+        digitalWrite(7, LOW);
+        digitalWrite(8, HIGH); // 8 HIGH
+        break;
+      
+      case '2': // 7 GREEN magnetic sensor off and the temperature is less than 25 degrees
+        Serial.println("The door is open. No one is there");
+        digitalWrite(6, LOW);
+        digitalWrite(7, HIGH); // 7 HIGH
+        digitalWrite(8, LOW);
+        break;
+      
+      default:
+        Serial.println("Value is unknown");
+        digitalWrite(6, LOW);
+        digitalWrite(7, LOW);
+        digitalWrite(8, LOW);
+        break;
+    }
+    delay(100); // delay 0.1 second
   }
 }
